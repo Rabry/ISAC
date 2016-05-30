1102ade82809f2308f68cacb6e97e7963275d06e
Rabry <ruberdavid@gmail.com>
2016-05-30 21:17:22 +0200

    Add files via upload

---
+++ layout/head.php
@@ -0,0 +1,21 @@
+<?php
+
+	echo '
+		<!DOCTYPE html>
+		<html>
+		  <head>
+		    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		    <link rel="stylesheet" type="text/css" href="layout/main.css" />
+		    <title> Mining </title>
+		        
+		    
+		  </head>
+		    
+		  <body>
+		  	<div id="wrapper">
+		  		<div id="header">
+		  			<a href="index.php?modul=mining&vypis_kategorie"><h1>Newspapper articles</h1></a>
+		  		</div>
+	';
+
+?>
